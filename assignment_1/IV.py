6a97a505d827977607454e4240af25971bdbb51a
Nikolai Malde <nikolaim@uia.no>
2025-08-25 20:42:04 +0200

    assignment 1

---
+++ assignment_1/IV.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+
+img_path = os.path.join(os.path.dirname(__file__), 'lena-1.png')
+image = cv2.imread(img_path)
+
+height, width, channels = image.shape
+size = image.size
+dtype = image.dtype
+
+print(f"Height: {height}")
+print(f"Width: {width}")
+print(f"Channels: {channels}")
+print(f"Size (number of values in the cubed array): {size}")
+print(f"Data type: {dtype}")
