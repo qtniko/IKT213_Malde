277b2a455730f19c6ff2229bb7dfe50413874a58
Nikolai Malde <nikolaim@uia.no>
2025-08-25 20:44:18 +0200

    assignment 1

--- assignment_1/IV.py
+++ assignment_1/IV.py
@@ -1,15 +1,21 @@
 import cv2
 import os
 
-img_path = os.path.join(os.path.dirname(__file__), 'lena-1.png')
-image = cv2.imread(img_path)
 
-height, width, channels = image.shape
-size = image.size
-dtype = image.dtype
+def main():
+    img_path = os.path.join(os.path.dirname(__file__), 'lena-1.png')
+    image = cv2.imread(img_path)
 
-print(f"Height: {height}")
-print(f"Width: {width}")
-print(f"Channels: {channels}")
-print(f"Size (number of values in the cubed array): {size}")
-print(f"Data type: {dtype}")
+    height, width, channels = image.shape
+    size = image.size
+    dtype = image.dtype
+
+    print(f"Height: {height}")
+    print(f"Width: {width}")
+    print(f"Channels: {channels}")
+    print(f"Size (number of values in the cubed array): {size}")
+    print(f"Data type: {dtype}")
+
+
+if __name__ == '__main__':
+    main()
--- assignment_1/V.py
+++ assignment_1/V.py
@@ -1,21 +1,27 @@
 import cv2
 import os
 
-fpath = os.path.join(os.path.dirname(__file__), 'camera_outputs.txt')
 
-cap = cv2.VideoCapture(0)
+def main():
+    fpath = os.path.join(os.path.dirname(__file__), 'camera_outputs.txt')
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+    cap = cv2.VideoCapture(0)
 
-fps = cap.get(cv2.CAP_PROP_FPS)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
 
-with open(fpath, "w") as f:
-    f.write(f"fps: {fps}\n")
-    f.write(f"width: {width}\n")
-    f.write(f"height: {height}\n")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-cap.release()
+    with open(fpath, "w") as f:
+        f.write(f"fps: {fps}\n")
+        f.write(f"width: {width}\n")
+        f.write(f"height: {height}\n")
+
+    cap.release()
+
+
+if __name__ == '__main__':
+    main()
